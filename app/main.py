1ab210f559077b34c2be56a800010fe801036c9e
richardSHkim <kshtrue95@gmail.com>
2022-09-17 17:44:28 +0900

    add schedule API

--- app/main.py
+++ app/main.py
@@ -1,8 +1,17 @@
 from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
+
+from app.routers import schedule
+from app.database import create_tables
 
 
 app = FastAPI()
+app.include_router(schedule.router)
+
+
+create_tables()
+
 
 @app.get("/")
 def read_root():
-    return {"Hello": "World"}
+    return {"Hello": "POMODOLIST BACKEND"}
---
+++ app/routers/schedule.py
@@ -0,0 +1,36 @@
+from fastapi import APIRouter
+
+from app.models.ScheduleModel import ScheduleModel
+from app.database import conn
+
+
+router = APIRouter(prefix='/api/schedule',
+                    tags=['schedule'])
+
+
+@router.get("/")
+def get_scedule():
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM schedules")
+    schedule = [x[0] for x in cur.fetchall()]
+    conn.commit()
+
+    return {"schedule": schedule}
+
+
+@router.post("/")
+def add_schedule(body: ScheduleModel):
+    cur = conn.cursor()
+    cur.execute("INSERT INTO schedules (time) VALUES (%s)" % str(body.time))
+    conn.commit()
+
+    return {"message": "success"}
+
+
+@router.delete("/")
+def clear_schedule():
+    cur = conn.cursor()
+    cur.execute("TRUNCATE TABLE schedules")
+    conn.commit()
+
+    return {"message": "success"}
